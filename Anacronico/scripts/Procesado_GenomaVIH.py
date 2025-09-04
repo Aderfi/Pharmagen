55872b8ede3e5ff654f18133e5d7549303f26937
Aderfi <zeooloo@gmail.com>
2025-09-04 20:39:00 +0200

    new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   Anacronico/Lib - Acceso directo.lnk 	new file:   Anacronico/__init__.py 	new file:   Anacronico/data/VIH_Genes_Dataframe.csv 	new file:   Anacronico/data/VIH_genome_selection.csv 	new file:   Anacronico/scripts/Procesado_GenomaVIH.py 	new file:   Anacronico/scripts/__init__.py 	new file:   Anacronico/scripts/search_viral_mutations.py 	new file:   Anacronico/src/__init__.py 	new file:   Anacronico/src/funciones.py 	new file:   main.py 	new file:   requirements.txt

---
+++ Anacronico/scripts/Procesado_GenomaVIH.py
@@ -0,0 +1,23 @@
+# Script en Python para el procesamiento de genomas de VIH
+# Autor: Astordna
+# Fecha: 2024-06-15
+# Descripción: Este script procesa los datos genómicos de VIH para análisis posteriores.
+#              Almacena el resultado del procesamiento en un archivo de salida. El script
+#              aisla las mutaciones relevantes y sus implicaciones farmacológicas en formato CSV.
+
+import os
+import pandas as pd
+from Bio import SeqIO
+
+# Datos que generar. Seleccion de genomas y mutaciones
+
+def procesar_genomas(input_genoma):
+
+    viral_fasta_seq = input_genoma
+    
+    SeqIO.
+    
+    
+    
+    
+    return
