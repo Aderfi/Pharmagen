f2cbeeb2d7d63f47a8d40907690ec62a4d2504b8
Aderfi <zeooloo@gmail.com>
2025-09-06 06:37:51 +0200

    Preparación de scripts y estructura del código - 06/09/2024

---
+++ Anacronico/scripts/viral_mutations_script.py
@@ -0,0 +1,28 @@
+# Este scipt tiene dos objetivos principales:
+# 1. Definir y almacenar un diccionario de mutaciones virales relevantes para el análisis de genomas de VIH.
+#       El almacenamiento se realiza mediante la importación de bases de datos oficiales
+# 2. Guardar este diccionario en un archivo CSV para su uso posterior en el análisis de genomas.
+# Autor: Astordna
+# Fecha: 2024-06-15
+
+import os
+import pandas as pd
+from Bio import SeqIO  # Importa el directorio principal
+import Anacronico 
+
+output_path = os.path.join(MAIN_DIR, 'data','mutaciones_VIH.csv')
+
+# Cada mutación será un diccionario con claves: 'Gen', 'Cadena de nucleótidos', 'Implicacion Farmacologica'
+datos_mutaciones = [12*[None] for _ in range(0)]  # Lista para almacenar los datos de las mutaciones
+
+
+mutations_df = pd.DataFrame(datos_mutaciones)
+
+# Crear el archivo CSV si no existe
+if not os.path.exists(output_path):
+    mutations_df.to_csv(output_path, index=False)
+
+
+# Guardar el DataFrame en el archivo CSV en modo append
+mutations_df.to_csv(output_path, mode='a', index=False)
+    
