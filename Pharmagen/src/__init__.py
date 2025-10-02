1a34fa353c160132e7da17927c575cdfabfa9ec8
Aderfi <zeooloo@gmail.com>
2025-10-02 20:46:13 +0200

    No se ya la vd

--- Pharmagen/src/__init__.py
+++ Pharmagen/src/__init__.py
@@ -8,10 +8,15 @@ de características genómicas relevantes.
 
 """
 
-from . import utils, data_handle, analysis, funciones, logger_config, project_config
+from . import utils, data_handle, analysis, funciones, logger_config, project_config, config
+from config import *
 
-__all__ = ["__analysis__", "__data_handle__", "__utils__", "__funciones__", "logger_config", "project_config"]
 __utils__ = ["utils"]
 __data_handle__ = ["io", "processing"]
 __funciones__ = ["mutations_extraction", "genoma_humano_process"]
 __analysis__ = ["modeling", "visualization"]
+__config__= [PHARMAGEN_DIR, LOGS_DIR, CACHE_DIR, SRC_DIR, \
+        RAW_DATA_DIR, PROCESSED_DATA_DIR, RESULTS_DIR, DOCS_DIR, SCRIPTS_DIR, ENV_SCRIPTS_DIR, \
+        DL_MODEL_DIR, MODEL_SCRIPTS_DIR, MODELS_DIR, MODEL_VOCABS_DIR, MODEL_DATA_DIR, MODEL_CSV_DIR, MODEL_JSON_DIR, \
+        CONFIG_FILE, LOG_FILE, AUTOR, VERSION, FECHA_CREACION \
+            ]
