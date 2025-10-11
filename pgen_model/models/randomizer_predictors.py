ddf581181ee5d715038f76ef336bd77ffbd3c588
Aderfi <zeooloo@gmail.com>
2025-10-11 20:46:22 +0200

    Olvide algo, arreglado

--- pgen_model/models/randomizer_predictors.py
+++ pgen_model/models/randomizer_predictors.py
@@ -11,8 +11,9 @@ i=0
 
 for drug, genes in drug_gene_dict.items():
     while i<50:
-        random_gene = np.random.choice(genes, size=np.random.randint(1, len(genes)+1), replace=False)
-        for gene in random_combi:
+        random_drug = np.random.choice(drug, size=np.random.randint(1, len(drug)+1), replace=False)
+        for gene in genes:
+            random_gene = np.random.choice(genes, size=np.random.randint(1, len(genes)+1), replace=False)
             df = pd.concat([df, pd.DataFrame({'Drug': [drug], 'Genotype': [gene]})], ignore_index=True)
         i += 1
 
