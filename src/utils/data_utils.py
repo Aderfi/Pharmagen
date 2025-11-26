fcea202630441ca9f6e754ea525eb7889e5ea815
Aderfi <zeooloo@gmail.com>
2025-11-26 20:37:21 +0100

    Testing changes on data input structure: Drugs --> Graphs instead of categorys (The intend is to try to represent the molecule as an incidence matrix, stablishing pretrained embedings for drugs and their chemical groups common structures (pharmacophore)

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -2,7 +2,8 @@
 import re
 import pandas as pd
 import numpy as np
-from typing import Any
+from typing import Any, cast
+
 
 RE_SPLITTERS = re.compile(r"[,;|]+")
 
@@ -22,3 +23,35 @@ def serialize_multilabel(val: Any) -> str:
         return str(val)
         
     return "|".join(sorted(parts))
+
+def normalize_drug_name(name):
+    import pubchempy as pcp
+    from pubchempy import Compound
+    
+    try:
+        # Busca el compuesto por nombre
+        compounds: list[Compound] = cast(list[Compound], pcp.get_compounds(name, 'error'))
+        
+        if compounds:
+            # Devuelve el primer resultado (normalmente el más relevante)
+            c = compounds[0]
+            return {
+                'Input': name,
+                'Synonyms': c.synonyms[:3], # Primeros 3 sinónimos
+                'IUPAC': c.iupac_name,
+                'SMILES': c.isomeric_smiles, # Útil para RDKit
+                'CID': c.cid
+            }
+    except Exception as e:
+        return None
+
+if __name__ == "__main__":
+
+    drug = "amphetamine"
+
+    norm_drug = normalize_drug_name(drug)
+
+    test = norm_drug
+
+    print(test)
+
